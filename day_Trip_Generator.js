edfc19b6b34371bd0f7f9b4de99bfe2ff68f10c8
Osc-53215 <you@example.com>
2021-03-16 18:03:17 -0500

    Have a semi-workin code. This is kinda working according to the pomt given to us just have to tweak it

--- day_Trip_Generator.js
+++ day_Trip_Generator.js
@@ -6,6 +6,7 @@ let randResturant = ['Culvers', 'Kopps', 'Olive Garden', 'Texas Roadhouse'];
 let randModeOfTransport = ['Uber', 'Lyft', 'Public Bus', 'Bycicle'];
 let randFormOfEntertainment = ['the Movies', 'an Open Mic Night', 'a Sporting Event', 'a Concert'];
 
+
 function randomGenerator(options){
     let randomString = Math.floor(Math.random()*options.length);
 
@@ -13,28 +14,62 @@ function randomGenerator(options){
 
     return item;
  }
+ 
 
 let result0 = randomGenerator(randDestination);
 console.log('For your first destination we choose...' + (result0));
+let anwser0 = confirm('Is this okay with you?')
+if (anwser0 == false){
+    console.log('Sorry about that, how about... ' + result0);
+} 
+else{
+    console.log('Lets continue! ')
+};
+
 
 alert(userName + ' now lets choose a Resturant to eat!');
 
 let result1 = randomGenerator(randResturant);
 console.log('For your Resturant we choose...' + (result1));
+let anwser1 = confirm('Is this okay with you?')
+if (anwser1 == false){
+    console.log('Sorry about that, how about... ' + result1);
+} 
+else{
+    console.log('Lets continue! ')
+};
+
 
 alert('Sweeeet! Next lets choose your form of transportation!');
 
 let result2 = randomGenerator(randModeOfTransport);
 console.log('How does taking a ' + (result2) + ' sound like to you?' );
+let anwser2 = confirm('Is this okay with you?')
+if (anwser2 == false){
+    console.log('Sorry about that, how about... ' + result2);
+} 
+else{
+    console.log('Lets continue! ')
+};
+
 
 alert('Awsome ' + userName + ' you are making this so easy for me!');
 alert('Last but not least lets choose a form of entertainment for you to have some fun!');
 
 let result3 = randomGenerator(randFormOfEntertainment);
 console.log('For entertainment we have ' + (result3));
+let anwser3 = confirm('Is this okay with you?')
+if (anwser3 == false){
+    console.log('Sorry about that, how about... ' + result3);
+} 
+else{
+    console.log('Lets continue! ')
+};
+
 
 console.log('Here is a summery of your trip! For your destination we have ' + (result0) + '. Next your place to eat was ' + (result1) +
 '.  For form of transportation we ended with ' + (result2) + '. Lastly we are going to end your night at ' + (result3) + '!');
+
 
 
 // console.log(userName + ' is this okay with you?');
