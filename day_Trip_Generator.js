8a6eb39ec413de5429be6bfca2566ccada6d3f27
Osc-53215 <you@example.com>
2021-03-15 21:46:10 -0500

    Succesfully created a unction to generate a randmon string for each array

--- day_Trip_Generator.js
+++ day_Trip_Generator.js
@@ -2,25 +2,18 @@ let randDestination = ['Milwaukee', 'Madison', 'Wisconsin Dells', 'Lake Geneva']
 let randResturant = ['Culvers', 'Kopps', 'Olive Garden', 'Texas Roadhouse'];
 let randModeOfTransport = ['Uber', 'Lyft', 'Public Bus', 'Bycicle'];
 let randFormOfEntertainment = ['Movies', 'Open Mic Night', 'Sporting Event', 'Concert'];
-let item = [0,1,2,3];
 
+function randomGenerator(options){
+    let randomString = Math.floor(Math.random()*options.length);
 
+    let item = options[randomString];
 
-
-function randomGenerator(randDestination, randResturant, randModeOfTransport, randFormOfEntertainment){
-    let randomNumber0 = Math.floor(Math.random()*randDestination.length);
-    let randomNumber1 = Math.floor(Math.random()*randResturant.length);
-    let randomNumber2 = Math.floor(Math.random()*randModeOfTransport.length);
-    let randomNumber3 = Math.floor(Math.random()*randFormOfEntertainment.length);
-
-   
-
-    let item = randDestination[randomNumber0];
-    let item = randResturant[randomNumber1];
-    let item = randModeOfTransport[randomNumber2];
-    let item = randFormOfEntertainment[randomNumber3];
     return item;
  }
 
- let result = randomGenerator(randDestination);
+ let result0 = randomGenerator(randDestination);
+ let result1 = randomGenerator(randResturant);
+ let result2 = randomGenerator(randResturant);
+ let result3 = randomGenerator(randResturant);
+
  console.log(result);
