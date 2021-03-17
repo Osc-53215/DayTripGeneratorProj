68d589184edd1e9e8280fdca60cee7a733377798
unknown <you@example.com>
2021-03-17 16:38:59 -0500

    Made a if-else statemtn to allow user to relected a given object if they where not content with the first option

--- day_Trip_Generator.js
+++ day_Trip_Generator.js
@@ -1,64 +1,68 @@
 let userName = prompt('What is your name? ');
-console.log('Hello ' + userName + ' This is your day trip generator!');
+alert('Hello ' + userName + ' This is your day trip generator!');
 
 let randDestination = ['Milwaukee', 'Madison', 'Wisconsin Dells', 'Lake Geneva'];
 let randResturant = ['Culvers', 'Kopps', 'Olive Garden', 'Texas Roadhouse'];
 let randModeOfTransport = ['Uber', 'Lyft', 'Public Bus', 'Bycicle'];
 let randFormOfEntertainment = ['the Movies', 'an Open Mic Night', 'a Sporting Event', 'a Concert'];
 
-
 function randomGenerator(options){
     let randomString = Math.floor(Math.random()*options.length);
-
     let item = options[randomString];
-
     return item;
  }
- 
 
 let result0 = randomGenerator(randDestination);
-console.log('For your first destination we choose...' + (result0));
-let anwser0 = confirm('Is this okay with you?');
+alert('For your first destination we choose...' + (result0));
+let expr = prompt('Is this okay with you? State yes or no. ')
+if(expr == 'yes'){
+    console.log('Lets continue!')
+}
+else {
+    alert('Okay how about...' + randomGenerator(randDestination))
+    prompt('Is this okay with you? State yes or no. ');
+    }
 
-let result01 = randomGenerator2(randDestination);
-if (anwser0 == false){
-    console.log('Sorry about that, how about... ' + result01);
-} 
-else{
-    console.log('Lets continue! ')
-};
-
+//Resturant 
 
 alert(userName + ' now lets choose a Resturant to eat!');
-
 let result1 = randomGenerator(randResturant);
-console.log('For your Resturant we choose...' + (result1));
-let anwser1 = confirm('Is this okay with you?')
+alert('For your Resturant we choose...' + (result1));
+let expr1 = prompt('Is this okay with you? State yes or no. ')
+if(expr1== 'yes'){
+    console.log('Lets continue!')
+}
+else {
+    alert('Okay how about...' + randomGenerator(randResturant))
+    prompt('Is this okay with you? State yes or no. ');
+    }
 
-let result02 = randomGenerator2(randDestination);
-if (anwser1 == false){
-    console.log('Sorry about that, how about... ' + result02);
-} 
-else{
-    console.log('Lets continue! ')
-};
-
+//break
 alert('Sweeeet! Next lets choose your form of transportation!');
+//break
+
+//Mode of transportiation
 
 let result2 = randomGenerator(randModeOfTransport);
-console.log('How does taking a ' + (result2) + ' sound like to you?' );
-let anwser2 = confirm('Is this okay with you?')
-if (anwser2 == false){
-    console.log('Sorry about that, how about... ' + result2);
-} 
-else{
-    console.log('Lets continue! ')
-};
+alert('How does taking a ' + (result2) + ' sound like to you?' );
+let expr2 = prompt('Is this okay with you? State yes or no. ')
+if(expr2 == 'yes'){
+    console.log('Lets continue!')
+}
+else {
+    alert('Okay how about...' + randomGenerator(randModeOfTransport))
+    prompt('Is this okay with you? State yes or no. ');
+    }
+
+
+
+
+
+
 
 
 alert('Awsome ' + userName + ' you are making this so easy for me!');
 alert('Last but not least lets choose a form of entertainment for you to have some fun!');
-
 let result3 = randomGenerator(randFormOfEntertainment);
 console.log('For entertainment we have ' + (result3));
 let anwser3 = confirm('Is this okay with you?')
@@ -66,28 +70,7 @@ if (anwser3 == false){
     console.log('Sorry about that, how about... ' + result3);
 } 
 else{
-    console.log('Lets continue! ')
+    console.log('')
 };
-
-
 console.log('Here is a summery of your trip! For your destination we have ' + (result0) + '. Next your place to eat was ' + (result1) +
 '.  For form of transportation we ended with ' + (result2) + '. Lastly we are going to end your night at ' + (result3) + '!');
-
-
-
-// console.log(userName + ' is this okay with you?');
-// let answer0 = prompt('yes or no?');
-
-// function satifactionWithResult(answer0){
-//     if (answer0 === yes){
-//         return true;
-//     }
-//     else if (anwser0 === no){
-//         console.log('Lets choose another location for you! How about ...' (result0));
-//     }
-//     else {
-//         return false;
-//     }
-
-    
-// }
